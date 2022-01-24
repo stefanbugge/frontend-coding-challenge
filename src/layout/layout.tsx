6da54aad101bfee3ec8a5a183060f3950f1c984c
Stefan Loeschcke <stefan@loeschcke.dk>
2022-01-24 10:05:15 +0100

    improve: adjusting body text contrast

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export function MainLayout(): React.ReactElement {
   return (
-    <div className="max-w-3xl mx-auto mt-28">
+    <div className="max-w-3xl mx-auto mt-28 text-body-text">
       <Outlet />
     </div>
   );
