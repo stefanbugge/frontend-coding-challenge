40672686f15eb5dccfcc3e2edea83027783d403d
Stefan Loeschcke <stefan@loeschcke.dk>
2022-01-24 22:29:31 +0100

    improve(list-skeleton): aligning list-skeleton component with list implementation

--- src/components/skeletons/list-skeleton.tsx
+++ src/components/skeletons/list-skeleton.tsx
@@ -1,26 +1,31 @@
 import React from "react";
 
+import { ListContainer } from "../list";
+
 export type ListSkeletonProps = {
   isLoaded: boolean;
-  rows?: number;
 };
+
+const widths = ["w-1/3", "w-1/2", "w-1/4", "w-7/12", "w-1/6"];
 
 export function ListSkeleton({
   isLoaded,
-  rows = 5,
   ...restProps
 }: React.PropsWithChildren<ListSkeletonProps>): React.ReactElement {
   if (isLoaded) {
     return <div {...restProps} />;
   }
   return (
-    <div className="animate-pulse flex flex-col space-y-2">
-      {Array.from(Array(rows).keys()).map((_, i) => (
-        <div key={i} className="flex space-x-2">
-          <div className="h-4 w-10 bg-gray-300 rounded" />
-          <div className="h-4 flex-grow bg-gray-300 rounded" />
-        </div>
+    <ListContainer>
+      {widths.map((w, i) => (
+        <li key={i} className="animate-pulse px-4 py-2 flex items-center">
+          <div className="flex-grow">
+            <div className={`${w} h-3 rounded bg-gray-300`} />
+            <div className="h-2 w-3/4 bg-gray-300 rounded mt-2" />
+          </div>
+          <div className="h-4 w-4 m-1 bg-gray-300 rounded" />
+        </li>
       ))}
-    </div>
+    </ListContainer>
   );
 }
