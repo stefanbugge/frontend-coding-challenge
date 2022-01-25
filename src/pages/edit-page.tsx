2ca5ad36f08fa47df746fc96f013e09b825dcb04
Stefan Loeschcke <stefan@loeschcke.dk>
2022-01-25 13:17:58 +0100

    fix(edit-page): resolve data for page before rendering contents

--- src/pages/edit-page.tsx
+++ src/pages/edit-page.tsx
@@ -2,27 +2,55 @@ import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import { Button } from "../components/button";
+import { PageSpinner } from "../components/spinner";
 import { PageHeading } from "../components/typography/headings";
 import {
+  Data,
   useDataQuery,
   useRemoveDataMutation,
   useUpdateDataMutation,
 } from "../fakeApollo";
+import { useUndoActions } from "../hooks/useUndo";
 import DataForm from "./components/form";
 import { FormProvider, FormState } from "./components/form-provider";
 
-export default function EditPage() {
+/**
+ * Resolve data by id before rendering the page
+ */
+export default function EditPageWrapper() {
+  const [data, setData] = React.useState<Data>();
   let { id } = useParams();
-  const { data } = useDataQuery();
+  const { data: dataList } = useDataQuery();
 
-  const initialFormState = React.useMemo(() => {
-    if (id == null) return undefined;
-    return (data ?? []).find((x) => x.id === id);
-  }, [id, data]);
+  React.useEffect(() => {
+    if (data !== undefined) {
+      // data already resolved
+      return;
+    }
+    if (!id) {
+      return;
+    }
+    const result = (dataList ?? []).find((x) => x.id === id);
+    if (result) {
+      setData(result);
+    }
+  }, [id, data, dataList]);
+
+  if (!data) return <PageSpinner />;
+  return <EditPage data={data} />;
+}
+
+type EditPageProps = {
+  data: Data;
+};
+
+function EditPage({ data }: EditPageProps) {
+  const { id } = data;
 
   const navigate = useNavigate();
   const [updateData, { loading: isUpdating }] = useUpdateDataMutation();
   const [deleteData, { loading: isDeleting }] = useRemoveDataMutation();
+  const { addUndoItem } = useUndoActions();
 
   const handleSubmit = async (state: FormState) => {
     if (id == null) return;
@@ -38,6 +66,7 @@ export default function EditPage() {
     if (id == null) return;
     try {
       await deleteData({ id });
+      addUndoItem(data);
       navigate("/", { replace: true });
     } catch {
       // Handle error
@@ -58,7 +87,7 @@ export default function EditPage() {
           </Button>
         </div>
       </div>
-      <FormProvider initialData={initialFormState} onSubmit={handleSubmit}>
+      <FormProvider initialData={data} onSubmit={handleSubmit}>
         <DataForm />
         <div className="flex justify-between mt-8">
           <Button
